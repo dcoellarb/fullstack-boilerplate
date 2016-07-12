d4e14f45634aa70bc0c5d29e25449a9a44418ad4
dcoellar <dcoellar@genexususa.com>
2016-07-12 12:43:50 -0500

    model complete

---
+++ react-web/src/model/enums/ModelCollections.js
@@ -0,0 +1,9 @@
+/**
+* ModelCollections enum of the
+* collections of the app
+*/
+const ModelCollections = {
+	/* Add collections here */
+};
+
+export default ModelCollections;
---
+++ react-web/src/model/models/modelsFactoryMethod.js
@@ -0,0 +1,14 @@
+import ModelCollections from './../enums/ModelCollections';
+
+export default (collection) => {
+    // Create the concrete model
+    /* Add each model creation here */
+    /* i.e. */
+    /*
+    if (collection === ModelCollections.proveedorCarga) {
+      return new ProveedorCarga();
+    } else if (collection === ModelCollections.user) {
+      return new User();
+    }
+    */
+};
